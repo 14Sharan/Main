6b3c3fa9c888900f9a50cfd28d9480748502b20a
Sharan <sharankaushal4474@gmail.com>
2023-04-01 19:11:26 +0530

    solve issue on subjects

--- course/urls.py
+++ course/urls.py
@@ -2,5 +2,7 @@ from django.urls import path,include
 from .views import *
 app_name='course'
 urlpatterns = [
-    path('subjects/',Subjects,name='subjects'),
+    path('subjects/',AllSubjects,name='subjects'),
+    path('add-subject/',AddSubject,name='add_subject'),
+    path('edit-subject/<int:id>',EditSubject,name='edit_subject'),
 ] 
--- course/views.py
+++ course/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from course.models import *
 
 
 @login_required
-def Subjects(request):
-    return render(request, "subjects/subjects-list.html")
+def AllSubjects(request):
+    subjects=Subjects.objects.all().order_by('-id')
+    return render(request, "subjects/subjects-list.html",{"subjects":subjects})
+
+
+@login_required
+def AddSubject(request):
+    if request.method == "GET":
+        return render(request,"subjects/add-subject.html")
+    if request.method == "POST":
+        Subjects.objects.create(title=request.POST.get('title'),
+                                description = request.POST.get('description'),
+                                subject_image = request.FILES.get('subject_image'))
+        return redirect('course:add_subject')
+    
+@login_required
+def EditSubject(request,id):
+    subject = Subjects.objects.get(id=id)
+    if request.method == "GET":
+        return render(request,"subjects/edit-subject.html",{"subject":subject})
+    if request.method == "POST":
+        if request.POST.get('title'):
+            subject.title = request.POST.get('title')
+        if request.POST.get('description'):
+            subject.description = request.POST.get('description')
+        if request.FILES.get('title'):
+            subject.subject_image = request.FILES.get('subject_image')
+        subject.save()
+        return redirect("course:subjects")
